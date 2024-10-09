f8fea75c960cd6bfaf381ad5021c7db97257ff1e
matcyr <cyrjuly30@gmail.com>
2024-10-09 15:36:00 +0800

    new file:   test_env.py

---
+++ test_env.py
@@ -0,0 +1,17 @@
+import torch
+# print(torch.__version__)
+print(f'PyTorch version: {torch.__version__}')
+import pandas
+# print(pandas.__version__)
+print(f'Pandas version: {pandas.__version__}')
+import numpy
+# print(numpy.__version__)
+print(f'Numpy version: {numpy.__version__}')
+import torch_geometric
+# print(torch_geometric.__version__)
+print(f'PyTorch Geometric version: {torch_geometric.__version__}')
+import rdkit
+# print(rdkit.__version__)
+print(f'RDKit version: {rdkit.__version__}')
+if __name__ == "__main__":
+    print("All packages are installed correctly")
